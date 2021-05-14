f0dc09ae9cd6291bceee6f99ca8b8f499a296774
Pasises <45708319+pasises@users.noreply.github.com>
2021-05-14 19:49:22 +0900

    feat : save file

--- scripts/dstar.py
+++ scripts/dstar.py
@@ -7,8 +7,8 @@
 import sys
 sys.path.append("../scripts/")
 from gridmap import *
-from matplotlib.animation import PillowWriter    #アニメーション保存用
-#%matplotlib notebook
+from matplotlib.animation import PillowWriter  # アニメーション保存用
+# %matplotlib notebook
 
 
 # In[2]:
@@ -16,42 +16,42 @@ from matplotlib.animation import PillowWriter    #アニメーション保存用
 
 class Dstar():
     def __init__(
-        self, world, sensing_range=3, 
+        self, world, sensing_range=3,
         drawCost_flag=False, drawPathToTake_flag=True, drawTakenPath_flag=True, drawMetricMap_flag=False
     ):
         self.world = world
-        self.real_grid_map = world.grid_map #実際のマップ
+        self.real_grid_map = world.grid_map  # 実際のマップ
         self.sensing_range = []
-        
+
         for i in range(-sensing_range, sensing_range):
             for j in range(-sensing_range, sensing_range):
                 if(np.sqrt(i**2 + j**2) > 3):
                     continue
                 self.sensing_range.append(np.array([i, j]))
-        
-        self.flag_path_draw = 0  #経路描画のフラグ
+
+        self.flag_path_draw = 0  # 経路描画のフラグ
         self.drawCost_flag = drawCost_flag
         self.drawPathToTake_flag = drawPathToTake_flag
         self.drawTakenPath_flag = drawTakenPath_flag
         self.drawMetricMap_flag = drawMetricMap_flag
-        
+
         self.initialize()
-        
+
     def initialize(self):
-        self.metric_grid_map = np.full(world.grid_map.shape, '-1')  #測定により得られたマップ
-        self.cost_map = np.full(world.grid_map.shape, 1)    #その地点が持つコスト
+        self.metric_grid_map = np.full(world.grid_map.shape, '-1')  # 測定により得られたマップ
+        self.cost_map = np.full(world.grid_map.shape, 1)  # その地点が持つコスト
         self.id_map = np.full(world.grid_map.shape, 0)
         self.child_id_map = np.full(world.grid_map.shape, 0)
         self.open_list = []
         self.closed_list = []
-        self.pathToTake = []   #今後の経路
+        self.pathToTake = []  # 今後の経路
         self.takenPath = []
         self.newObstaces = []
         self.h_map = np.full(world.grid_map.shape, 10000.0)
         self.k_map = np.full(world.grid_map.shape, 10000.0)
         self.currentIndex = self.world.start_index
-        
-        #各グリッドのIDを登録，ゴール地点のコストを決定
+
+        # 各グリッドのIDを登録，ゴール地点のコストを決定
         grid_id = 0
         for index, grid in np.ndenumerate(self.real_grid_map):
             self.id_map[index[0]][index[1]] = grid_id
@@ -62,8 +62,8 @@ class Dstar():
             elif(grid == "2" or self.world.isStart(index)):
                 self.metric_grid_map[index[0]][index[1]] = "1"
             grid_id += 1
-          
-        #既知の地図を用いて各グリッドのコストを生成し，経路を生成する
+
+        # 既知の地図を用いて各グリッドのコストを生成し，経路を生成する
         index = np.array([-1, -1])
         k_min = 0.0
         while(k_min != -1 and self.world.isStart(index)):
@@ -72,7 +72,7 @@ class Dstar():
                 k_min = -1
             else:
                 k_min = self.k(index)
-        
+
     def draw(self, ax, elems):
         self.checkNewObstacle(self.currentIndex)
         self.updateCost(self.currentIndex)
@@ -83,7 +83,7 @@ class Dstar():
         self.drawPathToTake(ax, elems) if(self.drawPathToTake_flag) else None
         self.drawRobot(ax, elems)
         self.currentIndex = self.next(self.currentIndex)
-       
+
     def plot(self, figsize=(4, 4), save_path=None):
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111)
@@ -97,15 +97,15 @@ class Dstar():
         for index, grid in np.ndenumerate(self.world.grid_map):
             if grid == '0':
                 self.world.drawGrid(index, "black", 1.0, ax)
-            if grid == '2' or self.world.isStart(index):  #Start
+            if grid == '2' or self.world.isStart(index):  # Start
                 self.world.drawGrid(index, "orange", 1.0, ax)
-            elif grid == '3' or self.world.isGoal(index):  #Goal 
+            elif grid == '3' or self.world.isGoal(index):  # Goal
                 self.world.drawGrid(index, "green", 1.0, ax)
-        
+
         for index in self.takenPath:
             if (not self.world.isStart(index)) and (not self.world.isGoal(index)):
                 self.world.drawGrid(index, "red", 0.5, ax)
-        
+
         plt.show()
 
         if(save_path is not None):
@@ -119,35 +119,35 @@ class Dstar():
             self.updateCost(self.currentIndex)
             self.currentIndex = self.next(self.currentIndex)
             self.takenPath.append(self.currentIndex)
-    
+
     def next(self, index):
         next_index = index
         if not self.world.isGoal(self.currentIndex):
             child_id = self.child_id_map[index[0]][index[1]]
-            child = np.where(self.id_map==child_id)
+            child = np.where(self.id_map == child_id)
             next_index = np.array([int(child[0]), int(child[1])])
         return next_index
-    
+
     def checkNewObstacle(self, currentIndex):
         self.newObstacles = []
-        for sensing_grid in self.sensing_range: #新しく障害物が見つかったかどうかを確認する
+        for sensing_grid in self.sensing_range:  # 新しく障害物が見つかったかどうかを確認する
             sensing_index = currentIndex + sensing_grid
             if(self.world.isOutOfBounds(sensing_index) or np.all(currentIndex == sensing_index)):
                 continue
-            if self.isUnobserved(sensing_index):  #まだ探索していないグリッドの場合
+            if self.isUnobserved(sensing_index):  # まだ探索していないグリッドの場合
                 self.metric_grid_map[sensing_index[0]][sensing_index[1]] = self.real_grid_map[sensing_index[0]][sensing_index[1]]
-                if self.isObservedObstacle(sensing_index):  #新しい障害物の場合
+                if self.isObservedObstacle(sensing_index):  # 新しい障害物の場合
                     self.insert(sensing_index, self.h(sensing_index))
-                    self.h_map[sensing_index[0]][sensing_index[1]] = 10000  #障害物のコストを無限大にする
-                    for adjacent_grid in neigbor_grids:  #周囲のグリッドをオープンリストに追加する
+                    self.h_map[sensing_index[0]][sensing_index[1]] = 10000  # 障害物のコストを無限大にする
+                    for adjacent_grid in neigbor_grids:  # 周囲のグリッドをオープンリストに追加する
                         adjacent_index = sensing_index + adjacent_grid
                         if self.world.isOutOfBounds(adjacent_index):
                             continue
-                        elif self.isObservedObstacle(adjacent_index): #障害物か
+                        elif self.isObservedObstacle(adjacent_index):  # 障害物か
                             continue
                         self.insert(adjacent_index, self.k(adjacent_index))
                     self.newObstacles.append(sensing_index)
-    
+
     def updateCost(self, currentIndex):
         k_min = 0.0
         h_now = self.h(currentIndex)
@@ -157,22 +157,22 @@ class Dstar():
                 k_min = -1
             else:
                 k_min = self.k(index)
-    
+
     def getPathToTake(self, currentIndex):
         self.pathToTake = []
         child_id = self.id(currentIndex)
-        while(child_id != self.id(self.world.goal_index)):  #ゴールになるまでたどる
-            child = np.where(self.id_map==child_id)
+        while(child_id != self.id(self.world.goal_index)):  # ゴールになるまでたどる
+            child = np.where(self.id_map == child_id)
             self.pathToTake.append([child[0][0], child[1][0]])
             child_id = self.child_id_map[int(child[0])][int(child[1])]
-    
+
     def processState(self):
         index = self.getMinKGrid()
         if(np.all(index == [-1, -1])):
             return np.array([-1, -1])
         k_old = self.k(index)
         self.closeAppend(index)
-        
+
         if(k_old < self.h(index)):
             for neigbor_grid in neigbor_grids:
                 neigbor_index = index + neigbor_grid
@@ -186,20 +186,20 @@ class Dstar():
                 ):
                     self.child_id_map[index[0]][index[1]] = self.id(neigbor_index)
                     self.h_map[index[0]][index[1]] = self.h(neigbor_index) + self.c(index, neigbor_index)
-        
+
         if(self.isHsame(k_old, self.h(index))):
             for neigbor_grid in neigbor_grids:
                 neigbor_index = index + neigbor_grid
                 if(self.world.isOutOfBounds(neigbor_index)):
                     continue
-                cost_h_new = self.h(index)+self.c(index, neigbor_index)
+                cost_h_new = self.h(index) + self.c(index, neigbor_index)
                 if(
                     self.isNewGrid(neigbor_index) or
                     (self.b(neigbor_index) == self.id(index) and self.isHsame(self.h(neigbor_index), cost_h_new) == False) or
                     (self.b(neigbor_index) != self.id(index) and self.isHlarger(self.h(neigbor_index), cost_h_new))
                 ):
                     self.child_id_map[neigbor_index[0]][neigbor_index[1]] = self.id(index)
-                    self.insert(neigbor_index, cost_h_new)                 
+                    self.insert(neigbor_index, cost_h_new)
         else:
             for neigbor_grid in neigbor_grids:
                 neigbor_index = index + neigbor_grid
@@ -226,9 +226,9 @@ class Dstar():
                     self.insert(neigbor_index, self.h(neigbor_index))
                 else:
                     pass
-        index = self.getMinKGrid()        
+        index = self.getMinKGrid()
         return index
-    
+
     def insert(self, index, h_new):
         if(h_new > 10000):
             h_new = 10000
@@ -247,19 +247,19 @@ class Dstar():
         self.openAppend(index, k)
         self.k_map[index[0]][index[1]] = k
         self.h_map[index[0]][index[1]] = h_new
-     
+
     def isOpenedGrid(self, index):
         if(self.id_map[index[0]][index[1]] in [val[0] for val in self.open_list]):
             return True
         else:
             return False
-        
+
     def isClosedGrid(self, index):
         if(self.id_map[index[0]][index[1]] in self.closed_list):
             return True
         else:
             return False
-        
+
     def isNewGrid(self, index):
         if(self.id_map[index[0]][index[1]] in [val[0] for val in self.open_list]):
             return False
@@ -267,7 +267,7 @@ class Dstar():
             return False
         else:
             return True
-       
+
     def openAppend(self, index, cost_k):
         if(self.isOpenedGrid(index) == True):
             if(self.k(index) != cost_k):
@@ -277,22 +277,22 @@ class Dstar():
         elif(self.isClosedGrid(index) == True):
             self.closed_list.remove(self.id(index))
         self.open_list.append([self.id(index), cost_k])
-    
+
     def closeAppend(self, index):
         if(self.isOpenedGrid(index) == True):
             self.open_list.remove([self.id(index), self.k(index)])
         elif(self.isClosedGrid(index) == True):
             return
         self.closed_list.append(self.id(index))
-    
+
     def getMinKGrid(self):
         if(self.open_list == []):
             return np.array([-1, -1])
-        val = np.argmin(self.open_list, axis=0) #評価マップの中から最も小さいもの抽出
+        val = np.argmin(self.open_list, axis=0)  # 評価マップの中から最も小さいもの抽出
         grid_id = self.open_list[val[1]][0]
-        index = np.where(self.id_map==grid_id)
+        index = np.where(self.id_map == grid_id)
         return np.array([index[0][0], index[1][0]])
-    
+
     def isHsame(self, cost_h1, cost_h2):
         if(cost_h1 == cost_h2):
             return True
@@ -301,7 +301,7 @@ class Dstar():
                 return True
             else:
                 return False
-    
+
     def isHlarger(self, cost_h1, cost_h2):
         if(cost_h1 >= 10000 and cost_h2 >= 10000):
             return False
@@ -310,17 +310,17 @@ class Dstar():
                 return True
             else:
                 return False
-    
+
     def h(self, index):
         return self.h_map[index[0]][index[1]]
-        
+
     def k(self, index):
         return self.k_map[index[0]][index[1]]
-    
-    def b(self, index):  #子ノードのidを返す
+
+    def b(self, index):  # 子ノードのidを返す
         return self.child_id_map[index[0]][index[1]]
-    
-    def c(self, index, neigbor_index): #隣接ノードへ移動するのに要するコスト
+
+    def c(self, index, neigbor_index):  # 隣接ノードへ移動するのに要するコスト
         if(self.metric_grid_map[index[0]][index[1]] == '0' or self.metric_grid_map[neigbor_index[0]][neigbor_index[1]] == '0'):
             return 10000
         else:
@@ -328,54 +328,54 @@ class Dstar():
 
     def id(self, index):
         return self.id_map[index[0]][index[1]]
-    
+
     def isUnobserved(self, index):
         if self.metric_grid_map[index[0]][index[1]] == '-1':
             return True
         else:
             return False
-    
+
     def isObservedObstacle(self, index):
         if self.metric_grid_map[index[0]][index[1]] == '0':
             return True
         else:
             return False
-    
+
     def drawNewObstacles(self, ax, elems):
         for index in self.newObstacles:
             self.world.drawGrid(index, "black", 1.0, ax, fill=True)
-    
+
     def drawPathToTake(self, ax, elems):
         for n in self.pathToTake:
             self.world.drawGrid(n, "red", 0.5, ax, elems=elems)
-    
+
     def drawRobot(self, ax, elems):
         if(self.id(self.currentIndex) != self.id(self.world.start_index)):
             self.world.drawGrid(self.currentIndex, "magenta", 0.5, ax) if(self.drawTakenPath_flag) else None
         self.world.drawGrid(self.currentIndex, "blue", 1.0, ax, elems=elems)
-    
+
     def drawCost(self, ax, elems):
         for index, grid in np.ndenumerate(self.metric_grid_map):
             if(self.k(index) < 10000):
-                c_num = int(self.k(index)) #Black→Blue
+                c_num = int(self.k(index))  # Black→Blue
                 c_num = int(c_num * 7.5)
-                if(c_num > 0xff): #Blue → Cyan
-                    c_num = (c_num-0xff)*16*16 + 0xff
-                    if(c_num > 0xffff): #Cyan → Green
-                        c_num = 0xffff - int((c_num-0x100ff)*4/256)
-                        if(c_num < 0xff00): #Green →Yellow
-                            c_num = (0xff00-c_num)*65536+0xff00
-                            if(c_num > 0xffff00): #Yellow → Red
-                                c_num = 0xffff00 - int((c_num-0xffff00)*0.5/65536)*256
+                if(c_num > 0xff):  # Blue → Cyan
+                    c_num = (c_num - 0xff) * 16 * 16 + 0xff
+                    if(c_num > 0xffff):  # Cyan → Green
+                        c_num = 0xffff - int((c_num - 0x100ff) * 4 / 256)
+                        if(c_num < 0xff00):  # Green →Yellow
+                            c_num = (0xff00 - c_num) * 65536 + 0xff00
+                            if(c_num > 0xffff00):  # Yellow → Red
+                                c_num = 0xffff00 - int((c_num - 0xffff00) * 0.5 / 65536) * 256
                 fill = True
                 alpha = 0.5
                 c = '#' + format(int(c_num), 'x').zfill(6)
                 self.world.drawGrid(index, c, alpha, ax, fill, elems)
             else:
-                #RAISE状態のセルを描画
+                # RAISE状態のセルを描画
                 #self.world.drawGrid(index, "red", 1.0, ax, True, elems)
                 continue
-                
+
     def drawMetricMap(self, ax, elems):
         for index, grid in np.ndenumerate(self.metric_grid_map):
             if(grid == "1"):
@@ -394,21 +394,17 @@ class Dstar():
 if __name__ == "__main__":
     time_span = 20
     time_interval = 0.1
-    
+
     grid_step = np.array([0.1, 0.1])
     grid_num = np.array([30, 30])
-    
+
     map_data = "../csvmap/map2.csv"
-    
+
     world = GridMapWorld(grid_step, grid_num, time_span, time_interval, map_data, time_show="time", debug=False, is_dynamic=True)
     world.append(Dstar(world))
-    
+
     world.draw()
-    #world.ani.save('dstar_map2_cost.gif', writer='pillow', fps=100)    #アニメーション保存
+    # world.ani.save('dstar_map2_cost.gif', writer='pillow', fps=100)    #アニメーション保存
 
 
 # In[ ]:
-
-
-
-
--- scripts/gridmap.py
+++ scripts/gridmap.py
@@ -35,8 +35,8 @@ class GridMapWorld():
         time_interval=0.1,
         map_data="map1.csv",
         debug=False,
-        isDynamic=False,
-        timeShow="step"
+        is_dynamic=False,
+        time_show="step"
     ):
         self.objects = []
         self.grid_step = np.array(grid_step)
@@ -45,89 +45,86 @@ class GridMapWorld():
         self.time_interval = time_interval
         self.map_data = map_data
         self.debug = debug
-        self.isDynamic = isDynamic
-        self.timeShow = timeShow
+        self.is_dynamic = is_dynamic
+        self.time_show = time_show
         self.step = 0
-
+        self.start_index = np.array([-1, -1])
+        self.goal_index = np.array([-1, -1])
+        
         with open(self.map_data) as f:
             reader = csv.reader(f)
             self.grid_map = np.array([row for row in reader]).T
-        self.start_index = np.array([-1, -1])
-        self.goal_index = np.array([-1, -1])
-
+        
         for index, grid in np.ndenumerate(self.grid_map):
-            if grid == '2':  # Start
+            if grid == '2':  #Start
                 self.start_index = np.array(index)
-            elif grid == '3':  # Goal
+            elif grid == '3':  #Goal
                 self.goal_index = np.array(index)
-
-    def append(self, obj):
+        
+    def append(self,obj):
         self.objects.append(obj)
-
+    
     def draw(self, figsize=(4, 4)):
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111)
         ax.set_aspect('equal')
-        ax.set_xlim(0, self.grid_step[0] * self.grid_num[0])
-        ax.set_ylim(0, self.grid_step[1] * self.grid_num[1])
-        ax.set_xlabel("X", fontsize=10)
-        ax.set_ylabel("Y", fontsize=10)
-
+        ax.set_xlim(0, self.grid_step[0]*self.grid_num[0])
+        ax.set_ylim(0, self.grid_step[1]*self.grid_num[1])
+        ax.set_xlabel("X",fontsize=10)
+        ax.set_ylabel("Y",fontsize=10)
+        
         for index, grid in np.ndenumerate(self.grid_map):
-            if(grid == '2' or self.isStart(index)):  # Start
+            if(grid == '2' or self.isStart(index)):   #Start
                 clr = "orange"
-            elif(grid == '3' or self.isGoal(index)):  # Goal
+            elif(grid == '3' or self.isGoal(index)):   #Goal
                 clr = "green"
-            elif(grid == '1'):  # Unoccupied
+            elif(grid == '1'):     #Unoccupied
                 continue
-            elif(grid == '0'):  # Obstacles
-                if(self.isDynamic):
+            elif(grid == '0'):   #Obstacles
+                if(self.is_dynamic):
                     clr = "lightgray"
                 else:
                     clr = "black"
             else:
                 continue
             self.drawGrid(index, clr, 1.0, ax)
-
+            
         elems = []
-
+        
         if self.debug:
-            for i in tqdm(range(int(self.time_span / self.time_interval))):
-                self.one_step(i, elems, ax)
+            for i in tqdm(range(int(self.time_span/self.time_interval))): self.one_step(i, elems, ax)
         else:
             self.ani = anm.FuncAnimation(
                 fig, self.one_step, fargs=(elems, ax),
-                frames=int(self.time_span / self.time_interval) + 1,
-                interval=int(self.time_interval * 1000),
+                frames=int(self.time_span/self.time_interval)+1,
+                interval=int(self.time_interval*1000),
                 repeat=False
             )
             plt.show()
-
+    
     def one_step(self, i, elems, ax):
-        while elems:
-            elems.pop().remove()
-
-        if(self.timeShow == "time"):
-            time_str = "t = %.2f[s]" % (self.time_interval * i)
-        elif(self.timeShow == "step"):
+        while elems: elems.pop().remove()
+        
+        if(self.time_show == "time"):
+            time_str = "t = %.2f[s]" % (self.time_interval*i)
+        elif(self.time_show == "step"):
             time_str = "step = " + str(i)
         else:
             time_str = ""
-
+            
         elems.append(
             ax.text(
-                self.grid_step[0] * self.grid_num[0] * 0.01,
-                self.grid_step[1] * self.grid_num[1] * 1.02,
+                self.grid_step[0]*self.grid_num[0]*0.01,
+                self.grid_step[1]*self.grid_num[1]*1.02,
                 time_str,
                 fontsize=10
             )
         )
-
+        
         for obj in self.objects:
             obj.draw(ax, elems)
-            if hasattr(obj, "one_step"):
-                obj.one_step(self.time_interval)
-
+            if hasattr(obj, "one_step"): obj.one_step(self.time_interval)
+    
     def drawGrid(self, index, color, alpha, ax, fill=True, elems=None):
         xy = index * self.grid_step
         r = patches.Rectangle(
@@ -142,7 +139,7 @@ class GridMapWorld():
             elems.append(ax.add_patch(r))
         else:
             ax.add_patch(r)
-
+    
     def isObstacle(self, index):
         if(self.isOutOfBounds(index)):
             return True
@@ -150,19 +147,19 @@ class GridMapWorld():
             return True
         else:
             return False
-
+        
     def isOutOfBounds(self, index):
         if(np.any(index >= self.grid_num) or np.any(index < [0, 0])):
             return True
         else:
             return False
-
+    
     def isStart(self, index):
         if(np.all(index == self.start_index)):
             return True
         else:
             return False
-
+    
     def isGoal(self, index):
         if(np.all(index == self.goal_index)):
             return True
@@ -182,10 +179,14 @@ if __name__ == "__main__":
 
     map_data = "csvmap/map1.csv"
 
-    world = GridMapWorld(grid_step, grid_num, time_span, time_interval, map_data, debug=False, isDynamic=False)
+    world = GridMapWorld(grid_step, grid_num, time_span, time_interval, map_data, debug=False, is_dynamic=False)
 
     world.draw()
-    # world.ani.save('input.gif', writer='pillow', fps=60)    #アニメーション保存
+    #world.ani.save('input.gif', writer='pillow', fps=60)    #アニメーション保存
 
 
 # In[ ]:
+
+
+
+
